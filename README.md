c9276f1687c99e7e1595b20645aece2bf63273a7
Alien <ztiany3@gmail.com>
2025-01-03 14:10:15 +0800

    update publishing configuration.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # android-livedata-ex
 
 ```groovy
-implementation "com.github.Ztiany:livedata-ex:1.0.3"
+implementation "io.github.ztiany:android-livedata-ex:1.2.0"
 ```
--- livedata-ex/src/main/java/com/android/base/foundation/livedata/LiveDataEx.kt
+++ livedata-ex/src/main/java/com/android/base/foundation/livedata/LiveDataEx.kt
@@ -2,13 +2,24 @@ package com.android.base.foundation.livedata
 
 import androidx.lifecycle.MutableLiveData
 
-fun <T> MutableLiveData<T>.init(t: T): MutableLiveData<T> {
+fun <T> MutableLiveData<T>.initWith(t: T): MutableLiveData<T> {
     this.postValue(t)
     return this
 }
 
 fun <T> MutableLiveData<T>.touchOff() {
     this.postValue(this.value)
+}
+
+fun <T> MutableLiveData<List<T>>.append(item: T) {
+    val value = this.value
+    if (value == null) {
+        this.postValue(listOf(item))
+    } else {
+        val mutableList = value.toMutableList()
+        mutableList.add(item)
+        this.postValue(mutableList)
+    }
 }
 
 fun <T> MutableLiveData<List<T>>.append(list: List<T>) {
